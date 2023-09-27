e650dc0007f85b178bd9c95c870bae1b567dd71a
Giulio Ricchelli <109143608+giulio-ricchelli-porini@users.noreply.github.com>
2023-09-27 18:42:22 +0200

    Update main.py

--- .github/actions/docker-action/main.py
+++ .github/actions/docker-action/main.py
@@ -4,7 +4,7 @@ import random
 name = os.environ["INPUT_NAME"]
 secret = os.environ["INPUT_SECRET"]
 
-print(f"I'm just giving a huge HELLO to {name}"}
-print(f"The secret is: {secret}"}
+print(f"I'm just giving a huge HELLO to {name}")
+print(f"The secret is: {secret}")
 with open(os.environ["GITHUB_OUTPUT"], "a") as o:
     print(f"random_number={random.random()}", file=o)
